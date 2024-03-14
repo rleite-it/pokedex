1e11adb2b142bbf41c702aef8aec8a4df5343c21
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-03-14 21:02:34 +0000

    fix(app): fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from 'App'
 describe('App component', () => {
   it('renders "POKEDEX" heading', () => {
     render(<App />)
-    const headingElement = screen.getByText('POKEDEX')
+    const headingElement = screen.getByText('Pokedex')
     expect(headingElement).toBeInTheDocument()
     expect(headingElement).toHaveClass('text-3xl font-bold underline')
   })
