24865a42c090589499d48978be1d9f00679a2dfb
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-03-14 20:18:24 +0000

    Add prettier, lint-staged and husky to the project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react'
+import App from 'App'
 
-describe("App component", () => {
-	it('renders "POKEDEX" heading', () => {
-		render(<App />);
-		const headingElement = screen.getByText("POKEDEX");
-		expect(headingElement).toBeInTheDocument();
-		expect(headingElement).toHaveClass("text-3xl font-bold underline");
-	});
-});
+describe('App component', () => {
+  it('renders "POKEDEX" heading', () => {
+    render(<App />)
+    const headingElement = screen.getByText('POKEDEX')
+    expect(headingElement).toBeInTheDocument()
+    expect(headingElement).toHaveClass('text-3xl font-bold underline')
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./tests/setup.ts",
-	},
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
 });
