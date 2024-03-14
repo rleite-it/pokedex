35453d4fb6d2184c66a24d4c689746f4e793f278
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-03-14 23:43:58 +0000

    feat: add browser router and routing (example)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Pokedex</h1>
+  return (
+    <>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path='/' element={<h1>HELLO</h1>} />
+        </Routes>
+      </Suspense>
+    </>
+  )
 }
 
 export default App
